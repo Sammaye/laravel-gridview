b165c39274afe7923739a187601189f61f6a6559
Sam Millman <sam.millman@googlemail.com>
2019-03-30 19:47:21 +0000

    Fixes for empty results and form submitting

--- src/views/grid.blade.php
+++ src/views/grid.blade.php
@@ -1,63 +1,69 @@
 <table {!! $grid->getAttributes() !!}>
     <thead>
-        @if($grid->getShowHeaderRow() && $grid->hasHeaderRow())
-            <tr {!! $grid->getHeaderRowAttributes() !!}>
-                @foreach($grid->getColumns() as $column)
-                    {!! $column->getHeaderCell() ?: $column->getEmptyHeaderCell() !!}
-                @endforeach
-            </tr>
-        @endif
-        @if($grid->getShowFilterRow() && $grid->hasFilterRow())
-            <tr {!! $grid->getFilterRowAttributes() !!} data-action="{!! $grid->getFilterFormActionUrl() !!}">
-                <form method="GET" action="{!! $grid->getFilterFormActionUrl() !!}">
+    @if($grid->getShowHeaderRow() && $grid->hasHeaderRow())
+        <tr {!! $grid->getHeaderRowAttributes() !!}>
+            @foreach($grid->getColumns() as $column)
+                {!! $column->getHeaderCell() ?: $column->getEmptyHeaderCell() !!}
+            @endforeach
+        </tr>
+    @endif
+    @if($grid->getShowFilterRow() && $grid->hasFilterRow())
+        <tr {!! $grid->getFilterRowAttributes() !!} data-action="{!! $grid->getFilterFormActionUrl() !!}">
+            <form method="GET" action="{!! $grid->getFilterFormActionUrl() !!}">
+                <input type="hidden"
+                       name="{{ $grid->getName() . '-' . $grid->getPageName() }}"
+                       id="{{ $grid->getName() . '-' . $grid->getPageName() }}"
+                       value="{{ $grid->getData()->currentPage() }}"
+                />
+                @if(request($grid->getName() . '-' . $grid->getSortName()))
                     <input type="hidden"
-                           name="{{ $grid->getName() . '-' . $grid->getPageName() }}"
-                           id="{{ $grid->getName() . '-' . $grid->getPageName() }}"
-                           value="{{ $grid->getData()->currentPage() }}"
+                           name="{{ $grid->getName() . '-' . $grid->getSortName() }}"
+                           id="{{ $grid->getName() . '-' . $grid->getSortName() }}"
+                           value="{{ request($grid->getName() . '-' . $grid->getSortName()) }}"
                     />
-                    @if(request($grid->getName() . '-' . $grid->getSortName()))
-                        <input type="hidden"
-                               name="{{ $grid->getName() . '-' . $grid->getSortName() }}"
-                               id="{{ $grid->getName() . '-' . $grid->getSortName() }}"
-                               value="{{ request($grid->getName() . '-' . $grid->getSortName()) }}"
-                        />
-                    @endif
-                    @foreach($column->getWithInput() as $input)
-                        @if(request($input))
+                @endif
+                @foreach($column->getWithInput() as $input)
+                    @if(request($input))
                         <input type="hidden"
                                name="{{ $input }}"
                                value="{{ request($input) }}"
                         />
-                        @endif
-                    @endforeach
-                    @foreach($grid->getColumns() as $column)
-                        {!! $column->getFilterCell() ?: $column->getEmptyFilterCell() !!}
-                    @endforeach
-                    <button type="submit" class="d-none">{{ __('Search') }}</button>
-                </form>
-            </tr>
-        @endif
+                    @endif
+                @endforeach
+                @foreach($grid->getColumns() as $column)
+                    {!! $column->getFilterCell() ?: $column->getEmptyFilterCell() !!}
+                @endforeach
+                <td class="d-none">
+                    <button type="submit" class="d-none">
+                        {{ __('Search') }}
+                    </button>
+                </td>
+            </form>
+        </tr>
+    @endif
     </thead>
     <tbody>
-        @forelse($grid->getData() as $row)
-            <tr {!! $grid->getDataRowAttributes($row) !!}>
-                @foreach($grid->getColumns() as $column)
-                    {!! $column->getDataCell($row) ?: $column->getEmptyDataCell($row) !!}
-                @endforeach
-            </tr>
-        @empty
-            <tr colspan="<?= count($grid->getColumns()) ?>">
+    @forelse($grid->getData() as $row)
+        <tr {!! $grid->getDataRowAttributes($row) !!}>
+            @foreach($grid->getColumns() as $column)
+                {!! $column->getDataCell($row) ?: $column->getEmptyDataCell($row) !!}
+            @endforeach
+        </tr>
+    @empty
+        <tr>
+            <td colspan="<?= count($grid->getColumns()) ?>">
                 {!! $grid->getEmptyData() !!}
-            </tr>
-        @endforelse
+            </td>
+        </tr>
+    @endforelse
     </tbody>
     @if($grid->getShowFooterRow() && $grid->hasFooterRow())
         <tfoot>
-            <tr {!! $grid->getFooterRowAttributes() !!}>
-                @foreach($grid->getColumns() as $column)
-                    {!! $column->getFooterCell() ?: $column->getEmptyFooterCell() !!}
-                @endforeach
-            </tr>
+        <tr {!! $grid->getFooterRowAttributes() !!}>
+            @foreach($grid->getColumns() as $column)
+                {!! $column->getFooterCell() ?: $column->getEmptyFooterCell() !!}
+            @endforeach
+        </tr>
         </tfoot>
     @endif
 </table>
